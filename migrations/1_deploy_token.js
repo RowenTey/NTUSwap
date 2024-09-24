d048073b15b58180139954ecabcfa5f3fd14cb24
rowentey <kaiseong02@gmail.com>
2024-09-24 23:54:19 +0800

    Update contract migrations

--- migrations/1_deploy_token.js
+++ migrations/1_deploy_token.js
@@ -1,6 +1,10 @@
-const Aura = artifacts.require("Aura");
+const Alpha = artifacts.require("Alpha");
+const Beta = artifacts.require("Beta");
+const Gamma = artifacts.require("Gamma");
 
 module.exports = function (deployer) {
-    const initialSupply = 1000000;
-    deployer.deploy(Aura, initialSupply);
+    const initialSupply = 1_000_000;
+    deployer.deploy(Alpha, initialSupply);
+    deployer.deploy(Beta, initialSupply);
+    deployer.deploy(Gamma, initialSupply);
 };
