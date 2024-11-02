9bcf33db08e5170d0cca0dc64b59775206d936be
AryanSethi20 <aryansethi20@gmail.com>
2024-11-02 20:26:42 +0800

    created market data

--- contracts/MarketData.sol
+++ contracts/MarketData.sol
@@ -1,9 +1,35 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+struct Market {
+    uint8 token1;
+    uint8 token2;
+}
+
 contract MarketData {
-    mapping(uint8 => Market) internal exchangeMarkets;
-    // Map market index to token index
-    mapping(bytes32 => uint8[]) internal marketTokens;
-    bytes32 internal marketIndex = 0;
+    // Maps market index to the market
+    mapping(uint16 => Market) internal exchangeMarkets;
+    uint16 internal marketId = 1;
+
+    event MarketCreatedEvent(
+        uint16 marketId,
+        uint8 token1,
+        uint8 token2,
+        uint256 timestamp
+    );
+
+    function addMarket(uint8 _tokenId) internal {
+        require(marketId + 1 > marketId, "Market index overflow");
+
+        for (uint8 i = 1; i < _tokenId; i++) {
+            exchangeMarkets[marketId] = Market({token1: i, token2: _tokenId});
+            marketId++;
+            emit MarketCreatedEvent(marketId, _tokenId, i, block.timestamp);
+        }
+    }
+
+    function getTokensFromMarketId(uint16 _marketId) internal view returns (uint8, uint8) {
+        Market memory market = exchangeMarkets[_marketId];
+        return (market.token1, market.token2);
+    }
 }
