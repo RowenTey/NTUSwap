43adc2809ff54268216a8f36ac0746b1a667f283
AryanSethi20 <aryansethi20@gmail.com>
2024-11-04 13:13:06 +0800

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -112,7 +112,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.13",
+      version: "0.8.20",
       settings: {
         optimizer: {
           enabled: true,
